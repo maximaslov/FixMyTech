d888148f6c5aa42966e3f981ccf8c463134f371c
Maxim <masmaxvad@gmil.com>
2023-03-08 14:17:19 +0200

    design done

---
+++ script.js
@@ -0,0 +1,12 @@
+// 'use strict'
+
+// const callbackBtn = document.querySelector('.callback-btn');
+
+// callbackBtn.addEventListener('click', scrollToSection);
+
+// function scrollToSection(e) {
+//     console.log(22)
+//     e.preventDefault();
+//     const section = document.querySelector(e.target.getAttribute("href"));
+//     section.scrollIntoView({ behavior: "smooth" });
+// }
