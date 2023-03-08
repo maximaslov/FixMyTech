fa5017b59eae7a45687e5a3f97bb5251a5117018
Maxim <masmaxvad@gmil.com>
2023-03-08 16:12:00 +0200

    done

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-// 'use strict'
+"use strict";
 
-// const callbackBtn = document.querySelector('.callback-btn');
+function sendMessage(event) {
+  event.preventDefault();
 
-// callbackBtn.addEventListener('click', scrollToSection);
+  const token = "5949737085:AAEiJnvBAzePqdtKNTqTCbYGJEOCjgAB9cI"; // Ваш токен бота
+  const chatId = "1976047730"; // ID чата, куда будут отправляться сообщения
+  const name = document.getElementById("name"); // Имя пользователя из формы
+  const phone = document.getElementById("phone"); // Телефон пользователя из формы
+  const message = document.getElementById("message"); // Сообщение пользователя из формы
 
-// function scrollToSection(e) {
-//     console.log(22)
-//     e.preventDefault();
-//     const section = document.querySelector(e.target.getAttribute("href"));
-//     section.scrollIntoView({ behavior: "smooth" });
-// }
+  const text = `Имя: ${name.value} ; Телефон: ${phone.value} ; Сообщение: ${message.value}`; // Форматируем текст сообщения
+
+  // Отправляем сообщение через API Telegram
+  fetch(
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .then(() => {
+      name.value = "";
+      phone.value = "";
+      message.value = "";
+    })
+    .catch((error) => console.error(error));
+}
+
+//   https://api.telegram.org/bot5949737085:AAEiJnvBAzePqdtKNTqTCbYGJEOCjgAB9cI/getUpdates
